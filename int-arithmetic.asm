decfb2d3668ca3e02278f5086dab60bc7a4a3f21
emilyrhee <emilyrhee99@gmail.com>
2024-09-20 15:27:15 -0400

    replaced bne with slt and bne

--- int-arithmetic.asm
+++ int-arithmetic.asm
@@ -15,32 +15,33 @@ result_labels:
 .globl main
 main:
     # print user prompt
-    lui $a0, 0x1001
-    addi $v0, $zero, 4
+    lui $a0, 0x1001			# load upper immediate value, I format, set upper 4 bytes of register to 0x1001 (defined data begins here)
+    addi $v0, $zero, 4		# add immediate value, I format, add 4 + 0 to $v0 to perform print string syscall
     syscall
 
 	# take input
-	addi $v0, $zero, 5
+	addi $v0, $zero, 5		# add immediate value, I format, add 5 + 0 to $v0 to perform read integer syscall
 	syscall
 	
 	# store input
-	add $t0, $v0, $zero
+	add $t0, $v0, $zero		# add, R format, add value from input (stored in $v0) + 0 to $t0
 	
 	# print user prompt
-    lui $a0, 0x1001
-    ori $a0, $a0, 0x0013
-    addi $v0, $zero, 4
+    lui $a0, 0x1001			# load upper immediate value, I format, set upper 4 bytes of register to 0x1001 (defined data begins here)
+    ori $a0, $a0, 0x0013	# or immediate, I, set lower 16 bits of register to 0x0013 to load "Enter another integer: " asciiz defined in .data
+    addi $v0, $zero, 4		# add immediate value, I format, add 4 + 0 to $v0 to perform print string syscall
     syscall
-	
+
 	# take input
-	addi $v0, $zero, 5
+	addi $v0, $zero, 5		# add immediate value, I format, add 5 + 0 to $v0 to perform read integer syscall
 	syscall
 
 	# store input
-	add $t1, $v0, $zero
+	add $t1, $v0, $zero		# add, R format, add value from input (stored in $v0) + 0 to $t0
 	
 	# check for larger number
-	bgt $t0, $t1, swap
+	slt $at, $t1, $t0
+	bne $at, $zero, swap
 	
 divide:
 	div $t1, $t0
@@ -95,7 +96,7 @@ results:
 	
 	# print remainder
 	lui $a0, 0x1001
-    ori $a0, $a0, 0x0059
+    ori $a0, $a0, 0x0059	# or immediate, I, set lower 16 bits of register to 0x0059 to load "Remainder: " asciiz defined in .data
 	addi $v0, $zero, 4
 	syscall
 	
