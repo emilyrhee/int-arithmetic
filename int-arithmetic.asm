11c7a06ad5a1eb984de2705a044830deb31c71f7
emilyrhee <emilyrhee99@gmail.com>
2024-09-20 14:33:26 -0400

    computes quotient and remainder then prints

--- int-arithmetic.asm
+++ int-arithmetic.asm
@@ -2,11 +2,13 @@
 
 .data
 prompts:
-    .asciiz "Enter an integer: " # 19 chars
+    .asciiz "Enter an integer: " # 19 chars (including null char), sum 0x13
     .asciiz "Enter another integer: " # 24 chars, sum 0x2B (43)
-	.asciiz "Larger integer: " 
-	.asciiz "Smaller integer: "
-	.asciiz "Quotient: "
+
+result_labels:
+	.asciiz "Larger integer: " # 17 chars, sum 0x3C (60)
+	.asciiz "Smaller integer: " # 18 chars, sum 0x4E (78)
+	.asciiz "Quotient: " # 11 chars, sum 0x59 (89)
 	.asciiz "Remainder: "
 
 .text
@@ -40,6 +42,11 @@ main:
 	# check for larger number
 	bgt $t0, $t1, swap
 	
+divide:
+	div $t1, $t0
+	mflo $t2
+	mfhi $t3
+	
 results:
 	# print larger int
     lui $a0, 0x1001
@@ -48,6 +55,51 @@ results:
 	syscall
 	
 	add $a0, $t1, $zero
+	addi $v0, $zero, 1
+	syscall
+	
+	# print new line
+	addi $a0, $zero, 10 # ASCII value of '\n' is 10
+	addi $v0, $zero, 11
+	syscall
+	
+	# print smaller int
+    lui $a0, 0x1001
+    ori $a0, $a0, 0x003C
+	addi $v0, $zero, 4
+	syscall
+
+	add $a0, $t0, $zero
+	addi $v0, $zero, 1
+	syscall
+	
+	# print new line
+	addi $a0, $zero, 10 # ASCII value of '\n' is 10
+	addi $v0, $zero, 11
+	syscall
+	
+	# print quotient
+	lui $a0, 0x1001
+    ori $a0, $a0, 0x004E
+	addi $v0, $zero, 4
+	syscall
+	
+	add $a0, $t2, $zero
+	addi $v0, $zero, 1
+	syscall
+	
+	# print new line
+	addi $a0, $zero, 10 # ASCII value of '\n' is 10
+	addi $v0, $zero, 11
+	syscall
+	
+	# print remainder
+	lui $a0, 0x1001
+    ori $a0, $a0, 0x0059
+	addi $v0, $zero, 4
+	syscall
+	
+	add $a0, $t3, $zero
 	addi $v0, $zero, 1
 	syscall
 	
@@ -62,4 +114,4 @@ swap:
 	add $t0, $t3, $zero
 	add $t1, $t2, $zero
 	
-	j results
+	j divide
